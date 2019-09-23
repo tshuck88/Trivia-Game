124396c5bb8ba7e49a0f865b91f27791088ab5b1
Tyler Shuck <tshuck88@gmail.com>
2019-09-23 16:14:27 -0700

    created array with the questions and answers

---
+++ assets/javascript/app.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+
+    var questionList = [
+        {
+            question1: {
+                text: "Which is the only NFL team to go a whole season undefeated, including the Super Bowl?",
+                answers: {
+                    correct: "1972 Miami Dolphins",
+                    incorrect1: "1985 Chicago Bears",
+                    incorrect2: "2007 New England Patriots",
+                    incorrect3: "2013 Seattle Seahawks",
+                }
+            },
+            question2: {
+                text: "How many NBA championships did Michael Jordan win with the Chicago Bulls?",
+                answers: {
+                    incorrect1: "10",
+                    incorrect2: "1",
+                    correct: "6",
+                    incorrect3: "3",
+                }
+            },
+            question3: {
+                text: "Which is the only team to play in every soccer World Cup tournament?",
+                answers: {
+                    incorrect1: "Spain",
+                    correct: "Brazil",
+                    incorrect2: "USA",
+                    incorrect3: "Portugal",
+                }
+            },
+            question4: {
+                text: "Which NFL team won the first two Super Bowls (in 1967 and 1968)?",
+                answers: {
+                    incorrect1: "Dallas Cowboys",
+                    correct: "Greenbay Packers",
+                    incorrect2: "Pittsburgh Steelers",
+                    incorrect3: "Seattle Seahawks",
+                }
+            },
+            question5: {
+                text: "Who is the NBA's all-time leading scorer?",
+                answers: {
+                    incorrect1: "Michael Jordan",
+                    incorrect2: "LeBron James",
+                    incorrect3: "Kobe Bryant",
+                    correct: "Kareem Abdul-Jabbar",
+                }
+            },
+            question6: {
+                text: "Who is the MLB's all-time leader in home runs?",
+                answers: {
+                    correct: "Barry Bonds",
+                    incorrect1: "Babe Ruth",
+                    incorrect2: "Mike Trout",
+                    incorrect3: "Alex Rodriguez",
+                }
+            },
+            question7: {
+                text: "Who is the NFL's all-time leader in touchdown passes",
+                answers: {
+                    incorrect1: "Brett Favre",
+                    incorrect2: "Tom Brady",
+                    incorrect3: "Aaron Rodgers",
+                    correct: "Peyton Manning",
+                }
+            },
+            question8: {
+                text: "Which NFL team has the most Super Bowl wins?",
+                answers: {
+                    incorrect1: "Seattle Seahawks",
+                    incorrect2: "San Francisco 49ers",
+                    correct: "New England Patriots",
+                    incorrect3: "Oakland Raiders",
+                }
+            }
+
+        }
+    ]
+
+});
