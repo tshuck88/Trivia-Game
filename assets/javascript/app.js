39c04ac625f20d956d87e32b047cb4d505c2d9e4
Tyler Shuck <tshuck88@gmail.com>
2019-09-23 21:17:05 -0700

    removed some boostrap styling. fixed the question list array, created a function to choose a new word at random, an initialize game function, display question text function, and a start button on click function

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,93 +2,87 @@ $(document).ready(function () {
 
     var questionList = [
         {
-            question1: {
-                text: "Which is the only NFL team to go a whole season undefeated, including the Super Bowl?",
-                answers: {
-                    correct: "1972 Miami Dolphins",
-                    incorrect1: "1985 Chicago Bears",
-                    incorrect2: "2007 New England Patriots",
-                    incorrect3: "2013 Seattle Seahawks",
-                }
-            }
+            text: "Which is the only NFL team to go a whole season undefeated, including the Super Bowl?",
+            answers: ["1972 Miami Dolphins", "1985 Chicago Bears", "2007 New England Patriots", "2013 Seattle Seahawks"],
+            correct: "1972 Miami Dolphins"
         },
         {
-            question2: {
-                text: "How many NBA championships did Michael Jordan win with the Chicago Bulls?",
-                answers: {
-                    incorrect1: "10",
-                    incorrect2: "1",
-                    correct: "6",
-                    incorrect3: "3",
-                }
-            }
+            text: "How many NBA championships did Michael Jordan win with the Chicago Bulls?",
+            answers: ["10", "1", "6", "3"],
+            correct: "6"
         },
         {
-            question3: {
-                text: "Which is the only team to play in every soccer World Cup tournament?",
-                answers: {
-                    incorrect1: "Spain",
-                    correct: "Brazil",
-                    incorrect2: "USA",
-                    incorrect3: "Portugal",
-                }
-            }
+            text: "Which is the only team to play in every soccer World Cup tournament?",
+            answers: ["Spain", "Brazil", "USA", "Portugal"],
+            correct: "Brazil"
         },
         {
-            question4: {
-                text: "Which NFL team won the first two Super Bowls (in 1967 and 1968)?",
-                answers: {
-                    incorrect1: "Dallas Cowboys",
-                    correct: "Greenbay Packers",
-                    incorrect2: "Pittsburgh Steelers",
-                    incorrect3: "Seattle Seahawks",
-                }
-            }
+            text: "Which NFL team won the first two Super Bowls (in 1967 and 1968)?",
+            answers: ["Dallas Cowboys", "Greenbay Packers", "Pittsburgh Steelers", "Seattle Seahawks"],
+            correct: "Greenbay Packers"
         },
         {
-            question5: {
-                text: "Who is the NBA's all-time leading scorer?",
-                answers: {
-                    incorrect1: "Michael Jordan",
-                    incorrect2: "LeBron James",
-                    incorrect3: "Kobe Bryant",
-                    correct: "Kareem Abdul-Jabbar",
-                }
-            }
+            text: "Who is the NBA's all-time leading scorer?",
+            answers: ["Michael Jordan", "LeBron James", "Kobe Bryant", "Kareem Abdul-Jabbar"],
+            correct: "Kareem Abdul-Jabbar"
         },
         {
-            question6: {
-                text: "Who is the MLB's all-time leader in home runs?",
-                answers: {
-                    correct: "Barry Bonds",
-                    incorrect1: "Babe Ruth",
-                    incorrect2: "Mike Trout",
-                    incorrect3: "Alex Rodriguez",
-                }
-            }
+            text: "Who is the MLB's all-time leader in home runs?",
+            answers: ["Barry Bonds", "Babe Ruth", "Mike Trout", "Alex Rodriguez"],
+            correct: "Barry Bonds"
         },
         {
-            question7: {
-                text: "Who is the NFL's all-time leader in touchdown passes",
-                answers: {
-                    incorrect1: "Brett Favre",
-                    incorrect2: "Tom Brady",
-                    incorrect3: "Aaron Rodgers",
-                    correct: "Peyton Manning",
-                }
-            }
+            text: "Who is the NFL's all-time leader in touchdown passes",
+            answers: ["Brett Favre", "Tom Brady", "Aaron Rodgers", "Peyton Manning"],
+            correct: "Peyton Manning"
         },
         {
-            question8: {
-                text: "Which NFL team has the most Super Bowl wins?",
-                answers: {
-                    incorrect1: "Seattle Seahawks",
-                    incorrect2: "San Francisco 49ers",
-                    correct: "New England Patriots",
-                    incorrect3: "Oakland Raiders",
-                }
-            }
+            text: "Which NFL team has the most Super Bowl wins?",
+            answers: ["Seattle Seahawks", "San Francisco 49ers", "New England Patriots", "Oakland Raiders"],
+            correct: "New England Patriots"
         }
-    ]
+    ];
+
+    var questionListCopy = questionList.slice(0);
+    var currentQuestion;
+    var correctAnswer;
+    var incorrectAnswer;
+    var unAnswered;
+    var timeRemaining;
+    var clockRunning = false;
+    var gameReset;
+    var gameOver;
+    
+    function chooseNewQuestion() {
+        var index = Math.floor(Math.random() * questionList.length)
+        currentQuestion = questionList[index];
+        questionList.splice(index, 1)
+        displayNewQuestion()
+    };
+
+    function initializeGame() {
+        questionListCopy
+        questionList = questionListCopy.slice(0)
+        timeRemaining = 30;
+        correctAnswer = 0;
+        incorrectAnswer = 0;
+        unAnswered = 0;
+        clockRunning = true;
+        chooseNewQuestion();
+    };
+
+    function displayNewQuestion() {
+        $("#time-text").text("Time Remaining: " + timeRemaining + " seconds");
+        $("#question-text").text(currentQuestion.text);
+        $("#answer-a-text").text(currentQuestion.answers[0]);
+        $("#answer-b-text").text(currentQuestion.answers[1]);
+        $("#answer-c-text").text(currentQuestion.answers[2]);
+        $("#answer-d-text").text(currentQuestion.answers[3]);
+    };
+
+    $(document).on("click", "#start-button", function () {
+        initializeGame();
+        $("#start-button").hide();
+    })
 
 });
