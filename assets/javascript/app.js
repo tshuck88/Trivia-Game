66fc9fd79e69fbda92941855549f38d509256240
Tyler Shuck <tshuck88@gmail.com>
2019-09-24 14:56:04 -0700

    created a function to check if the game is over. added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    // array of question objects
     var questionList = [
         {
             question: "Which is the only NFL team to go a whole season undefeated, including the Super Bowl?",
@@ -51,61 +52,59 @@ $(document).ready(function () {
         }
     ];
 
-    var questionListCopy = questionList.slice(0);
+    // global variables
+    var questionListCopy = questionList.slice(0); // sets a copy of the original question list that will not be mutated
     var currentQuestion;
     var correctAnswer;
     var incorrectAnswer;
     var unAnswered;
     var timeRemaining;
-    var clockRunning = false;
     var intervalId;
 
+    // function to choose a new question
     function chooseNewQuestion() {
-        var index = Math.floor(Math.random() * questionList.length)
-        currentQuestion = questionList[index];
-        questionList.splice(index, 1);
+        var index = Math.floor(Math.random() * questionList.length); // creates a random number between 0 and the length of questionList, stores it as an index variable
+        currentQuestion = questionList[index]; // uses the index variable just created to set the current question from questionList
+        questionList.splice(index, 1); // removes the chosen question from questionList so it cannot be chosen again
         startTime();
         clearText();
         displayNewQuestion();
-
     };
 
+    // function to begin/reset the game
     function initializeGame() {
-        questionList = questionListCopy.slice(0)
-        timeRemaining = 30;
+        questionList = questionListCopy.slice(0); // resets questionList to the unmutated questionListCopy
+        timeRemaining = 30; 
         correctAnswer = 0;
         incorrectAnswer = 0;
         unAnswered = 0;
         chooseNewQuestion();
     };
 
+    // function to start time
     function startTime() {
-        clockRunning = true;
         timeRemaining = 30;
-        intervalId = setInterval(function () {
-            $("#time-text").text("Time Remaining: " + timeRemaining + " seconds");
-            timeRemaining--;
-            if (timeRemaining < 0) {
+        intervalId = setInterval(function () { // runs the follow code by the interval amount of 1 second
+            timeRemaining--; // decreases time
+            $("#time-text").text("Time Remaining: " + timeRemaining + " seconds"); // updates time remaining game text
+            if (timeRemaining < 0) { // if time remaining reaches 0 
                 stopTime();
-                unAnswered++;
+                unAnswered++; // increases unanswered count by 1
                 clearText();
-                $("#question-text").html("<h2>Out of Time!</h2>");
-                $("#answer-a").html("<h2>The correct answer was " + currentQuestion.correct + "</h2>");
-                $("#answer-d").html('<img src="' + currentQuestion.gif + '" width="auto" height="200px">')
-                if (correctAnswer + incorrectAnswer + unAnswered === questionListCopy.length) {
-                    setTimeout(gameOver, 5000)
-                } else {
-                    setTimeout(chooseNewQuestion, 5000);
-                }
+                $("#question-text").html("<h2>Out of Time!</h2>"); // tells the user they ran out of time
+                $("#answer-a").html("<h2>The correct answer was " + currentQuestion.correct + "</h2>"); // displays correct answer
+                $("#answer-d").html('<img src="' + currentQuestion.gif + '" width="auto" height="200px">') // displays correct answer gif
+                isGameOver(); // calls function to check if the game is over
             }
         }, 1000)
     };
-
+    
+    //function to stop time
     function stopTime() {
-        clearInterval(intervalId)
-        clockRunning = false;
+        clearInterval(intervalId);
     };
 
+    // function to update game text and display new question and answers
     function displayNewQuestion() {
         $("#time-text").text("Time Remaining: " + timeRemaining + " seconds");
         $("#question-text").text(currentQuestion.question);
@@ -119,18 +118,20 @@ $(document).ready(function () {
         $("#answer-d-text").text(currentQuestion.answers[3]);
     };
 
+    // function to clear all game text
     function clearText() {
-        $("#finished-text").text("")
-        $("#correct-text").text("")
-        $("#incorrect-text").text("")
-        $("#unanswered-text").text("")
-        $("#question-text").text("")
+        $("#finished-text").text("");
+        $("#correct-text").text("");
+        $("#incorrect-text").text("");
+        $("#unanswered-text").text("");
+        $("#question-text").text("");
         $("#answer-a").text("");
         $("#answer-b").text("");
         $("#answer-c").text("");
         $("#answer-d").text("");
     };
 
+    // function for when the game is over
     function gameOver() {
         clearText();
         $("#finished-text").text("All done, here's how you did:");
@@ -138,37 +139,40 @@ $(document).ready(function () {
         $("#incorrect-text").text("Incorrect Answers: " + incorrectAnswer);
         $("#unanswered-text").text("Unanswered: " + unAnswered);
         $("#start-button").show().text("Start Over?");
-    }
+    };
+    
+    // function to check if the game is over
+    function isGameOver(){
+        if(correctAnswer + incorrectAnswer + unAnswered === questionListCopy.length) { // if the amount of user answers is equal to amount of possible questions
+            setTimeout(gameOver, 5000) // then the game is over, runs the game over function after 5 seconds
+        } else {
+            setTimeout(chooseNewQuestion, 5000); // if not, then chooses a new question after 5 seconds
+        }
+    };
 
+    // function to start the game and hide the start button on start button click
     $(document).on("click", "#start-button", function () {
         initializeGame();
         $("#start-button").hide();
     });
 
+    // function to respond to the users choice
     $(document).on("click", ".answer-button", function () {
-        if ($(this).text() === currentQuestion.correct) {
-            stopTime();
-            correctAnswer++;
+        if ($(this).text() === currentQuestion.correct) { // if button the user clicks on matches the correct answer of the current question
+            stopTime(); 
+            correctAnswer++; // increase correct answers count by 1
             clearText();
-            $("#question-text").html("<h2>Correct!</h2>");
-            $("#answer-d").html('<img src="' + currentQuestion.gif + '" width="auto" height="200px">')
-            if (correctAnswer + incorrectAnswer + unAnswered === questionListCopy.length) {
-                setTimeout(gameOver, 5000)
-            } else {
-                setTimeout(chooseNewQuestion, 5000);
-            }
-        } else {
+            $("#question-text").html("<h2>Correct!</h2>"); // tells user they are correct
+            $("#answer-d").html('<img src="' + currentQuestion.gif + '" width="auto" height="200px">') // displays correct answer gif
+            isGameOver(); // calls function to check if the game is over
+        } else { // if the user choice is not correct
             stopTime();
-            incorrectAnswer++;
+            incorrectAnswer++; // increase incorrect answers count by 1
             clearText();
-            $("#question-text").html("<h2>Nope!</h2>");
-            $("#answer-a").html("<h2>The correct answer was " + currentQuestion.correct + "</h2>");
-            $("#answer-d").html('<img src="' + currentQuestion.gif + '" width="auto" height="200px">')
-            if (correctAnswer + incorrectAnswer + unAnswered === questionListCopy.length) {
-                setTimeout(gameOver, 5000)
-            } else {
-                setTimeout(chooseNewQuestion, 5000);
-            }
+            $("#question-text").html("<h2>Nope!</h2>"); // tells user they are incorrect
+            $("#answer-a").html("<h2>The correct answer was " + currentQuestion.correct + "</h2>"); // displays correct answer
+            $("#answer-d").html('<img src="' + currentQuestion.gif + '" width="auto" height="200px">') // displays correct answer gif
+            isGameOver(); // calls function to check if the game is over
         }
     });
 });
